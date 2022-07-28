d71dbc4431875e3a1f1dee3336827c790ce47dc7
Alireza Kamyab <alirezakamyab19@gmail.com>
2022-07-28 12:29:33 +0430

    Adds regularization and bug fixes

--- Linear Regression/linear_regression_util.py
+++ Linear Regression/linear_regression_util.py
@@ -16,12 +16,21 @@ def predict(x, w, b):
 # y is the vector of training outputs
 # w is a vector of the weight for each feature
 # b is a scalar for bias
-def cost(X, y, w, b):
-    m, _ = X.shape
-    sq_err_sum = 0
+# lambda is regularization parameter
+def cost(X, y, w, b, lambda_ = 0):
+    m, n = X.shape
+    cost_sum = 0
     for i in range(m):
-        sq_err_sum += (predict(X[i], w, b) - y[i]) ** 2
-    return sq_err_sum / (2 * m)
+        err = predict(X[i], w, b) - y[i]
+        cost_sum += err ** 2
+    cost_sum = cost_sum / (2 * m)
+    
+    reg_sum = 0
+    for j in range(n):
+        reg_sum += w[j] ** 2
+    reg_sum = reg_sum * (lambda_ / (2 * m))
+    
+    return cost_sum + reg_sum
 
 
 # X is a matrix consisting of m rows (entries) and n columns (features)
@@ -30,17 +39,23 @@ def cost(X, y, w, b):
 # b is a scalar for bias
 # dj_dw a vector which is the derivative of the cost function with respect to each feature
 # dj_db a scalar which is the derivative of the cost function with respect to b
-def gradient(X, y, w, b):
+# lambda is regularization parameter
+def gradient(X, y, w, b, lambda_= 0):
+    m, n = X.shape
     dj_dw = np.zeros(n)
     dj_db = 0
-    m, n = X.shape
+    
     for i in range(m):
-        err = predict(X[i], w, b) - y[i]
+        err = function(X[i], w, b) - y[i]
         for j in range(n):
-            dj_dw[j] = dj_dw[j] + (err * X[i][j])
-        dj_db = dj_db + err
+            dj_dw[j] = dj_dw[j] + err * X[i][j]
+        dj_db += err
     dj_dw = dj_dw / m
     dj_db = dj_db / m
+    
+    for j in range(n):
+        dj_dw[j] = dj_dw[j] + (lambda_ / m) * w[j]
+    
     return dj_dw, dj_db
 
 
@@ -51,44 +66,38 @@ def gradient(X, y, w, b):
 # alpha scalar, learning rate
 # iterations, number of updates that should be made
 # hist, history of every update
-def gradient_descent(X, y, init_w, init_b, alpha, iterations=1000):
+# lambda is regularization parameter
+def gradient_descent(X, y, init_w, init_b, alpha, cost_function, gradient_function, iterations = 1000, lambda_ = 0):
     w = copy.deepcopy(init_w)
     b = init_b
-    hist = []
-    for i in range(1, iterations):
-        dj_dw, dj_db = gradient(X, y, w, b)
-        w = w - (alpha * dj_dw)
-        b = b - (alpha * dj_db)
-        hist.append[[i, w, b, cost(X, y, w, b)]]
-    return w, b, np.array(hist)
+    
+    for i in range(1, iterations + 1):
+        dj_dw, dj_db = gradient_function(X, y, w, b, lambda_)
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+        
+        if i % 1000 == 0:
+            print(f'i: {i} w={w} b={b} cost={cost_function(X, y, w, b)}')
+    return w, b
 
 
 # X is a matrix consisting of m rows (entries) and n columns (features)
 def z_normalization(X):
-    x_norm = copy.deepcopy(X)
-    m, n = x_norm.T.shape
-    for i in range(m):
-        std = x_norm[i].std()
-        mean = x_norm[i].mean()
-        x_norm[i] = (x_norm[i] - mean) / std
-    return x_norm.T
+    mu = np.mean(X, axis=0)
+    sigma = np.mean(X, axis=0)
+    x_norm = (X - mu) / sigma
+    return x_norm, mu, sigma
 
 
 # X is a matrix consisting of m rows (entries) and n columns (features)
 def mean_normalization(X):
-    x_norm = copy.deepcopy(X)
-    m, n = x_norm.T.shape
-    for i in range(m):
-        mean = x_norm[i].mean()
-        x_norm[i] = x_norm[i] / mean
-    return x_norm.T
+    mu = np.mean(X, axis=0)
+    x_mean = X / mu
+    return x_mean, mu
 
 
 # X is a matrix consisting of m rows (entries) and n columns (features)
 def scale(X):
-    x_norm = copy.deepcopy(X)
-    m, n = x_norm.T.shape
-    for i in range(m):
-        max_value = x_norm[i].max()
-        x_norm[i] = x_norm[i] / max_value
-    return x_norm.T
+    abs_max = np.max(np.abs(X), axis=0)
+    x_scaled = X / abs_max
+    return x_scaled, abs_max
